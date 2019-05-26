12e396ba4c38ea1bd41d13c4b5b5154f35a1511d
shuyu <359369982@qq.com>
2019-05-26 22:23:31 +0800

    增加权限处理

--- app/src/main/java/com/shuyu/app/MainActivity.java
+++ app/src/main/java/com/shuyu/app/MainActivity.java
@@ -1,14 +1,19 @@
 package com.shuyu.app;
 
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import permissions.dispatcher.PermissionUtils;
 
 /**
  * Created by shuyu on 2016/11/15.
@@ -19,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.main_frameLayout)
     FrameLayout mainFrameLayout;
+
+
+    final String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,23 @@ public class MainActivity extends AppCompatActivity {
         transaction.addToBackStack(null);
         transaction.commit();
 
+        boolean hadPermission = PermissionUtils.hasSelfPermissions(this, permissions);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !hadPermission) {
+            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO};
+            requestPermissions(permissions, 1110);
+        }
+
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean sdPermissionResult = PermissionUtils.verifyPermissions(grantResults);
+        if (!sdPermissionResult) {
+            Toast.makeText(this, "没获取到sd卡和录音权限，无法正常运行哦", Toast.LENGTH_LONG).show();
+        }
+    }
+
 
 }
